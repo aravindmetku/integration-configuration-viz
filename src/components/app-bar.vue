5383a6b286c597ca3520b7b62cb5bdbca8aa897d
AravindMetku-Leanix <aravind.metku@leanix.net>
2021-05-18 21:45:59 +0530

    added shadow for app bar. Started neumorphic changes for right side

--- src/components/app-bar.vue
+++ src/components/app-bar.vue
@@ -19,6 +19,7 @@ export default {
     padding: 10px;
     background: rgb(44,118,194);
     background: linear-gradient(90deg, rgba(44,118,194,1) 0%, rgba(0,121,255,1) 100%);
+    box-shadow: -9px -13px 62px -7px rgba(0,0,0,0.48);
   }
 
   .title {
